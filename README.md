7477cf178c6271517002ccf22339bc8b6521e4ff
Cheng Jingxuan <hbxycjx@outlook.com>
2024-10-26 23:05:20 +0800

    Introduced a Couple of Scripts to Enforce Font

---
+++ README.md
@@ -0,0 +1,54 @@
+## Font Definition Sheet
+
+This sheet contains all fonts in **CommilitiaAsset**.
+
+https://cdn.jsdelivr.net/gh/CommilitiaAsset/CSSStyle/fonts.css
+
+## Sarasa Gothic SC Rewrite
+
+This script forces all labels to use `Sarasa Gothic SC` font and `MonoLisa Commilitia` as code font.
+
+https://cdn.jsdelivr.net/gh/CommilitiaAsset/CSSStyle/generic_rewrite_gothic_sc.js
+
+## Sarasa Gothic TC Rewrite
+
+This script forces all labels to use `Sarasa Gothic TC` font and `MonoLisa Commilitia` as code font.
+
+https://cdn.jsdelivr.net/gh/CommilitiaAsset/CSSStyle/generic_rewrite_gothic_tc.js
+
+## Sarasa UI SC Rewrite
+
+This script forces all labels to use `Sarasa UI SC` font and `MonoLisa Commilitia` as code font.
+
+https://cdn.jsdelivr.net/gh/CommilitiaAsset/CSSStyle/generic_rewrite_ui_sc.js
+
+## Sarasa UI TC Rewrite
+
+This script forces all labels to use `Sarasa UI TC` font and `MonoLisa Commilitia` as code font.
+
+https://cdn.jsdelivr.net/gh/CommilitiaAsset/CSSStyle/generic_rewrite_ui_tc.js
+
+## Automatic Gothic Rewrite
+
+This script forces all labels to use `Sarasa Gothic SC` or `TC` font (based on `lang` property on `html` label) and `MonoLisa Commilitia` as code font.
+
+https://cdn.jsdelivr.net/gh/CommilitiaAsset/CSSStyle/generic_rewrite_gothic.js
+
+## Automatic UI Rewrite
+
+This script forces all labels to use `Sarasa UI SC` or `TC` font (based on `lang` property on `html` label) and `MonoLisa Commilitia` as code font.
+
+https://cdn.jsdelivr.net/gh/CommilitiaAsset/CSSStyle/generic_rewrite_ui.js
+
+## Note
+
+- Sarasa Gothic v1.0.22; MonoLisa 2.015.
+- The scripts could be performance heavy because they would set styles for every label and create an event listener to track all new labels.
+
+- We have acquired personal license for MonoLisa and all relevant files are for personal, non-commercial use. You should ensure to be licensed before using the style sheet or scripts.
+
+- Credits:
+  - https://www.monolisa.dev/
+  - https://github.com/be5invis/Sarasa-Gothic
+
+- You are free to use any part of the sheet/scripts, as long as it does not violate copyright or licenses of any other third party.
---
+++ generic_rewrite_ui_sc.js
@@ -0,0 +1,158 @@
+(function () {
+  const unicodeFont = "Sarasa UI SC";
+
+  const codeLabels = [
+    "code",
+    "pre",
+    "samp",
+    "pre *",
+    "kbd",
+    "tt",
+    "datalist",
+    "progress",
+    "meter",
+    "var"
+  ].join(", ");
+
+  function rgbToHsl(color) {
+    let [ r, g, b ] = color.map(c => c / 255);
+    const max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2;
+
+    if (max === min){
+      h = s = 0;
+    } else {
+      const d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch (max) {
+        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+        case g: h = (b - r) / d + 2; break;
+        case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+    }
+    
+    return [ h, s, l ];
+  }
+
+  function hueToRgb(p, q, t) {
+    if (t < 0) {
+      t += 1;
+    }
+    if (t > 1) {
+      t -= 1;
+    }
+    if (t < 1/6) {
+      return p + (q - p) * 6 * t;
+    }
+    if (t < 1/2) {
+      return q;
+    }
+    if (t < 2/3) {
+      return p + (q - p) * (2/3 - t) * 6;
+    }
+    return p;
+  }
+
+  function hslToRgb(color) {
+    const [ r, g, b ] = color;
+
+    if (s === 0) {
+      r = g = b = l;
+    } else {
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hueToRgb(p, q, h + 1/3);
+      g = hueToRgb(p, q, h);
+      b = hueToRgb(p, q, h - 1/3);
+    }
+
+    return [ Math.round(r * 255), Math.round(g * 255), Math.round(b * 255) ];
+  }
+
+  function rgbToHex(color) {
+    return `#${color.map(c => c.toString(16).padStart(2, "0")).join("")}`;
+  }
+
+  function parseColor(str) {
+    let match;
+
+    if (str.startsWith("rgba")) {
+      match = str.match(/^rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+(\.?\d*)?)\)$/);
+      return [ parseInt(match[1]), parseInt(match[2]), parseInt(match[3]), parseFloat(match[4]) ];
+    } else if (str.startsWith("rgb")) {
+      match = str.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+      return [ parseInt(match[1]), parseInt(match[2]), parseInt(match[3]), 255 ];
+    } if (str.startsWith("#")) {
+      switch(str.length) {
+        case 9: return [ parseInt(str.slice(1, 3), 16), parseInt(str.slice(3, 5), 16), parseInt(str.slice(5, 7), 16), parseInt(str.slice(7, 9), 16) ];
+        case 7: return [ parseInt(str.slice(1, 3), 16), parseInt(str.slice(3, 5), 16), parseInt(str.slice(5, 7), 16), 255 ];
+        case 5: return [ parseInt(str[1] + str[1], 16), parseInt(str[2] + str[2], 16), parseInt(str[3] + str[3], 16), parseInt(str[4] + str[4], 16) ];
+        case 4: return [ parseInt(str[1] + str[1], 16), parseInt(str[2] + str[2], 16), parseInt(str[3] + str[3], 16), 255 ];
+      }
+    }
+
+    return [ 0, 0, 0, 255 ];
+  }
+
+  function getShadowedColor(node) {
+    let [ r, g, b, a ] = parseColor(getComputedStyle(node).color);
+    let [ h, s, l ] = rgbToHsl([ r, g, b ]);
+    l = 0.486 * l + 0.514;
+    a = a * 0.87;
+
+    [ r, g, b ] = hslToRgb([ h, s, l ]);
+    return rgbToHex(r, g, b, a);
+  }
+  
+  function applyStyle(node) {
+    if (node.matches(codeLabels)) {
+      node.style.fontFamily = `'MonoLisa Commilitia', '${unicodeFont}'`;
+      node.style.webkitTextStroke = "0.08px";
+      node.style.textShadow = `0.45px 0.45px 0 ${getShadowedColor(node)}`;
+      return;
+    }
+    node.style.fontFamily = `'${unicodeFont}'`;
+    node.style.webkitTextStroke = "0.08px";
+    node.style.textShadow = `0.45px 0.45px 0 ${getShadowedColor(node)}`;
+  }
+
+  function executeOnAllChild(node, selector, callback) {
+    callback(node);
+    node.querySelectorAll(selector).forEach(child => callback(child));
+  }
+
+  function selectOrChild(node, selector) {
+    return node.matches(selector) || node.querySelector(selector);
+  }
+
+  function onMutation(node) {
+    if (!(node instanceof HTMLElement)) {
+      return;
+    }
+    executeOnAllChild(node, "*", applyStyle);
+  }
+
+  async function onDomLoaded() {
+    await Promise.resolve();
+    document.querySelectorAll("*").forEach(node => {
+      if (node instanceof HTMLElement) {
+        applyStyle(node);
+      }
+    });
+
+    new MutationObserver(mutation => mutation.addedNodes.forEach(onMutation))
+      .observe(
+        document.body,
+        { attributes: false, childList: true, subtree: true }
+      );
+  }
+
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = "https://cdn.jsdelivr.net/gh/CommilitiaAsset/CSSStyle/fonts.css";
+  document.head.appendChild(link);
+
+  document.addEventListener("DOMContentLoaded", onDomLoaded);
+})();
